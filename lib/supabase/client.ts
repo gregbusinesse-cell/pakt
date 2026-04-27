ad8c94fad97886591ee385537bea585558fa2f7b
ton pseudo <tonemail@gmail.com>
2026-04-27 21:43:31 +0200

    fix supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,11 @@
 // lib/supabase/client.ts
 // Client-side Supabase instance
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserClient } from '@supabase/ssr'
 import type { Database } from '@/lib/supabase/types'
 
-export const createClient = () => createClientComponentClient<Database>()
+export const createClient = () =>
+  createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
